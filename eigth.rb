a18b93201aa5f0ac3348ae582edb46f7088d2986
Mukunth-arya <mukunthnataraj@gmail.com>
2022-02-16 22:12:43 +0530

    version2

---
+++ eigth.rb
@@ -0,0 +1,16 @@
+puts("enter the name dude")
+name = gets.chomp()
+puts("enter the age dude")
+age = gets.chomp() 
+
+puts ("My name is:"+name)
+puts ("My age is:"+age)
+
+data = Array["key", "value"]
+puts data
+puts data[0]
+puts data[0,1] 
+puts data.length()
+value = [1,2,3]
+puts value[1].abs()
+puts math.sqrt(value)
