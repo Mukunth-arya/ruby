1f719e3283b07b389def0bbb3906d3ad4e746b48
Mukunth-arya <mukunthnataraj@gmail.com>
2022-02-28 23:46:20 +0530

    Data new

---
+++ case_statement.rb
@@ -0,0 +1,21 @@
+def value1()
+
+   puts "enter the below value"
+   datavalue = Integer(gets) 
+  
+     
+
+   case datavalue
+   when 1
+     print "hey buddy"
+   when 2
+     print "hey dude"   
+   else
+     print "please enter the proper value"
+   end
+    
+    
+
+end
+
+puts value1()
