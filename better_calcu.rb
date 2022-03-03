6e06dd77232eea53c60a96fbfbc8ca41f49bbfb2
Mukunth-arya <mukunthnataraj@gmail.com>
2022-03-03 23:03:52 +0530

    version10

---
+++ better_calcu.rb
@@ -0,0 +1,13 @@
+data1 = "mukunth"
+data2 = "" 
+data3 = 0
+data4 = 5
+
+while data2 != data1
+    if data3 < data4
+        puts ("enter the name below")
+        data2 = gets.chomp()
+        print ("welcome mukunth\n")
+        data3 = data3+1
+    end
+end
